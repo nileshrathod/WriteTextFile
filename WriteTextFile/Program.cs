1f4481311661bbbb61cd591e1eadc87522bff260
nileshrathod <njrathod@gmail.com>
2017-01-18 18:40:37 +0530

    First Commit in GitHub

---
+++ WriteTextFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteTextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name = DateTime.Now.ToString("HH-mm-ss-ffffff").ToString() + "-" + "Sample.txt";
+            
+            string path = Path.Combine(@"C:\Code Files", name);
+            
+            using (StreamWriter sw = File.CreateText(path))
+            {
+               sw.WriteLine("This is just a test text for sample file.");
+            }
+
+        }
+    }
+}
